00cc8d9d09c97f6cc97a7483d4ab9b688108ce54
ellyruthsilberstein <47394491+ellyruthsilberstein@users.noreply.github.com>
2019-02-06 11:47:27 -0500

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,46 +1,4 @@
-/*************************************************************************
- *
- *  Pace University
- *  Fall 2018
- *  Data Structures and Algorithms
- *
- *  Course: CS 241
- *  Team members: Elly
- *  Collaborators: NONE
- *  References: NONE
- *
- *  Assignment: #2
- *  Problem: Write a program using queues
- *  Description: I created a Queue class using the basic methods we discussed in class and then
- *  *  made a test class to implement the methods
- *
- *  Input: N/A
- *  Output: N/A
- *
- *  Visible data fields:
- *  - LinkedList<T> myQueue
- *
- *  Visible methods:
- *  - public void enqueue(T item)
- *  - public void dequeue()
- *  - public boolean inQueue(T item)
- *  - public boolean isEmpty()
- *  - public String toString()
- *
- *
- *   Remarks
- *   N/A
- *
- *   Question 3:
- *
- *   Run times for each method:
- *   - public void enqueue(T item): O(1) because you are just adding something to the queue, you don't have to iterate through
- *   - public void dequeue(): O(1) because you are just removing from the back of the queue, you don't have to iterate through
- *   - public boolean inQueue(T item): O(n) because you have to search through the queue which is n items. 
- *   - public boolean isEmpty(): O(1) because you just have to check the first value to see if the queue is empty. 
- *   - public String toString(): O(1) because it is a simple toString method.
- *
- *************************************************************************/
+//Queue
 import java.util.*;
 
 public class Queue <T> {
